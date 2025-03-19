af34f183d2b6acf1c79c553c8b0fa623d1d98c2c
Gabriel Dias <gsd_gabriel@hotmail.com>
2025-03-19 00:03:43 -0300

    Estruturando a página utlizando HTML e incluindo funções nos botões

--- contact/admin.py
+++ contact/admin.py
@@ -4,13 +4,13 @@ from contact import models
 @admin.register(models.Contact)
 class Contact(admin.ModelAdmin):
     #colocando uma tabela na área adminstrativa
-    list_display = 'id', 'first_name', 'last_name', 'phone', 'email',
-    ordering = 'id',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'email', 'show'
+    ordering = '-id',
     #list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 100
-    #list_editable = 'first_name', 'last_name',
+    list_editable = 'show',
     list_display_links = 'id', 'first_name', 'last_name', 'phone', 'email',
 
 @admin.register(models.Category)
